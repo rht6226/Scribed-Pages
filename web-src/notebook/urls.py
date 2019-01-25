31b79b2e98154b58298cbefba7c91979a41a44dc
rht6226 <rohitraazanand567@gmail.com>
2019-01-26 05:00:39 +0530

    View Saved Notebook Create New Articles

--- web-src/notebook/urls.py
+++ web-src/notebook/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import home, create_notebook
+from .views import home, create_notebook, view_notebook
 
 urlpatterns = [
       path('', home, name='home'),
       path('notebook/create', create_notebook, name='create_notebook'),
-      # path('notebook/view', view_notebook, name='view_notebook'),
+      path('notebook/view/<slug:uid>', view_notebook, name='view_notebook'),
 ]
